65715d803c03ec864ee86e3c3182fd72bca45137
Vasa2609 <vasilstifurak303@gmail.com>
2024-11-01 10:44:21 +0200

    Дз за 05.10

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-
+    path('name/', views.name, name='name'),
+    path('age/', views.age, name='age'),
+    path('hobby/', views.hobby, name='hobby')
 ]
